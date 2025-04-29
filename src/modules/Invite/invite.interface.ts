774efe5849a590f35f962e3160c624c985387f66
hoqradwan <hmdradwanul@gmail.com>
2025-04-29 17:57:11 +0600

    invite interface

--- src/modules/Invite/invite.interface.ts
+++ src/modules/Invite/invite.interface.ts
@@ -4,15 +4,13 @@ export interface IInvite {
     image?: string;
     appointmentDate: Date;
     time: string;
-    duration: number; // Duration in minutes
+    duration: number; 
     expirationDateTime: Date;
     agenda: string;
-    participants: Types.ObjectId[]; // Array of User references
-    restaurant: Types.ObjectId; // Reference to Restaurant
-    menuItem: Types.ObjectId; // Reference to Menu
+    participants: Types.ObjectId[]; 
+    restaurant: Types.ObjectId; 
+    menuItem: Types.ObjectId; 
     contribution: 'Each pay their own' | 'Organizer pay for all' | 'Participants pay organizer';
     amount: number;
     status: 'Pending' | 'Completed' | 'Cancelled';
-    createdAt?: Date; // Automatically added by Mongoose timestamps
-    updatedAt?: Date; // Automatically added by Mongoose timestamps
 }
